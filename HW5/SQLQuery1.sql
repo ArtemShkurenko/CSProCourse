46c744c868ee95a9e2925bda5f1888ef9a459b48
Artyom Shkurenko <artnorm@ukr.net>
2023-04-18 08:10:08 +0300

    HW5 correct#1

--- HW5/SQLQuery1.sql
+++ HW5/SQLQuery1.sql
@@ -1,37 +1,58 @@
-Create DATABASE Logistics
+IF EXISTS 
+   (
+   SELECT name FROM master.dbo.sysdatabases 
+   WHERE name = N'Logistics'
+   )
+BEGIN    
+   PRINT 'Database already exist.'
+END
+ELSE
+BEGIN
+   CREATE DATABASE Logistics;
+   PRINT 'Database created successfully.'
+END
+
 USE Logistics;
-create table Warehouse (
- Id INT PRIMARY KEY,
- Name VARCHAR(50) NOT NULL);
-CREATE TABLE Vehicle (
-Id INT PRIMARY KEY,
-Name VARCHAR(50) NOT NULL,
-VehicleType VARCHAR(50) DEFAULT 'Car' NOT NULL,
-MaxCargoWeightKg INT NOT NULL,
-MaxCargoVolume DECIMAL (10,2)
-);
+GO
+
+CREATE TABLE Warehouse (
+    Id INT PRIMARY KEY,
+    Name VARCHAR(50) NOT NULL);
+ GO
+ 
+ CREATE TABLE Vehicle (
+      Id INT PRIMARY KEY,
+      Name VARCHAR(50) NOT NULL,
+      VehicleType VARCHAR(50) DEFAULT 'Car' NOT NULL,
+      MaxCargoWeightKg INT NOT NULL,
+      MaxCargoVolume DECIMAL (10, 2) NOT NULL);
+GO
+    
 CREATE TABLE Cargo (
-Id UNIQUEIDENTIFIER PRIMARY KEY,
-Code VARCHAR(50) NOT NULL,
-Volume DECIMAL(10,2) NOT NULL,
-Weight INT NOT NULL,
-WarehouseId INT NOT NULL,
-VehicleId INT NOT NULL,
-FOREIGN KEY (WarehouseId) REFERENCES Warehouse(Id),
-FOREIGN KEY (VehicleId) REFERENCES Vehicle(Id)
-);
-ALTER TABLE Vehicle
-ALTER COLUMN MaxCargoVolume DECIMAL(10, 2) NOT NULL;
+     Id UNIQUEIDENTIFIER PRIMARY KEY,
+     Code VARCHAR(50) NOT NULL,
+     Volume DECIMAL(10, 2) NOT NULL,
+     Weight INT NOT NULL,
+     WarehouseId INT NULL DEFAULT NULL,
+     VehicleId INT NULL DEFAULT NULL,
+     FOREIGN KEY (WarehouseId) REFERENCES Warehouse(Id),
+     FOREIGN KEY (VehicleId) REFERENCES Vehicle(Id)
+     );
+GO
 
 CREATE TABLE Invoice (
-Id UNIQUEIDENTIFIER PRIMARY KEY,
-RecipientAddress VARCHAR(50) NOT NULL,
-SenderAddress VARCHAR(50) NOT NULL,
-RecipientPhoneNumber VARCHAR(50),
-SenderPhoneNumber VARCHAR(50),
-CargoId UNIQUEIDENTIFIER,
-FOREIGN KEY (CargoId) REFERENCES Cargo(Id)
-);
+     Id UNIQUEIDENTIFIER PRIMARY KEY,
+     RecipientAddress VARCHAR(50) NOT NULL,
+     SenderAddress VARCHAR(50) NOT NULL,
+     RecipientPhoneNumber VARCHAR(50),
+     SenderPhoneNumber VARCHAR(50),
+     CargoId UNIQUEIDENTIFIER,
+     FOREIGN KEY (CargoId) REFERENCES Cargo(Id)
+     );
+GO
+
+
+
 
 
 
